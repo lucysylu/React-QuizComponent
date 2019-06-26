ea0f474a9714d78b5cf1a3d94dffe063b617ff99
Lucy A. Lu <lucy.a.lu@avanade.com>
2019-06-26 15:03:31 -0400

    task 6

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,7 @@ class Quiz extends Component {
         return(
 
             <div>
-                {isQuizEnd ? <QuizEnd /> :
+                {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> :
                 <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/> }
             </div>
         );
@@ -24,6 +24,12 @@ class Quiz extends Component {
     showNextQuestion(){
         this.setState((state) => {
             return {quiz_position: state.quiz_position + 1}
+        })
+    }
+
+    handleResetClick(){
+        this.setState( () => {
+            return {quiz_position: 1}   
         })
     }
 }
