c012619af0e91b67233a47bb3e99a5dbe8fd3730
Lucy A. Lu <lucy.a.lu@avanade.com>
2019-06-26 11:11:12 -0400

    task 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+
+    constructor(props){
+      super(props);
+      this.state = {incorrectAnswer: false}
+    }
     render(){
         return(
             <main>
@@ -15,13 +20,22 @@ class QuizQuestion extends Component {
                 })}
               </ul>
             </section>
+            {this.state.incorrectAnswer ? <p className="error"> SOrry, that's not right</p> : null}
           </main>
         )
     }
 
     handleClick(buttonText){
       if (buttonText === this.props.quiz_question.answer){
+        this.setState( () => {
+          return {incorrectAnswer: false}
+        });
         this.props.showNextQuestionHandler();
+      }
+      else {
+        this.setState( () => {
+          return {incorrectAnswer: true}
+        });
       }
     }
 }
